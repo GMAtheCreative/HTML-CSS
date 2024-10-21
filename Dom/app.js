e954141ac1f895ddb5842f6467bf9081de8f2fb5
GMAtheCreative <timothywonder01@gmail.com>
2024-10-21 17:23:25 +0100

    progress

--- Dom/app.js
+++ Dom/app.js
@@ -1,46 +1,52 @@
-// const wrapper = document.getElementById("wrapper");
-// console.log(wrapper);
-// const bookTitles = document.getElementsByClassName("name")
-// console.log(bookTitles);
-// // Array.from(bookTitles)
-// // console.log(Array.isArray(bookTitles))
-// const arrayResult = Array.from(bookTitles);
+const bookList = document.querySelector("#book-list ul");
 
-// arrayResult.forEach((element) => {
-//     console.log(element.textContent)
-// });
-
-// const lis = document.getElementsByTagName('li')
-// console.log(lis)
-
-// const pageBanner = document.querySelector("#page-banner")
-// console.log(pageBanner)
-
-// const booToRead = document.querySelectorAll(".title")
-// console.log(booToRead)
-// booToRead.forEach(Element => (
-//     console.log(Element)
-// ))
-
-// const pageBanner = document.querySelector(".name");
-// console.log(pageBanner.childElementCount)
-
-// const bookTitle = document.querySelectorAll(".name");
-
-// bookTitle.forEach(element => {
-//     console.log(element.textContent)
-//     console.log(element.textContent += " text");
-    
-// })
-
-
-//we are now wor king on delete...
-//query to get exactly what you want to delete
-const theuls = document.querySelector("#book-list ul");
-
-console.log(theuls)
-
-//event listener to to target click`
-theuls.addEventListener('click', (event) => {
-    console.log(event)
+bookList.addEventListener('click', (event) => {
+    const deleteButton = event.target.className
+    if (deleteButton == "delete") {
+        const deletedBtn = event.target.parentNode;
+        bookList.removeChild(deletedBtn)
+    }
 })
+
+
+const bookForm = document.forms['add-book']
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const theInput = bookForm.querySelector('input[type ="text"]').value.trim();
+
+  if (theInput != "") {
+    const newLi = document.createElement('li');
+    const newBookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+    // console.log()
+    newBookName.textContent = theInput;
+    deleteBtn.textContent = 'delete';
+
+    newBookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    newLi.appendChild(newBookName);
+    newLi.appendChild(deleteBtn);
+    bookList.appendChild(newLi);
+  }
+      
+    bookForm.querySelector('input[type ="text"]').value = "";
+
+})
+
+const searchForm = document.forms['search-books'].querySelector('input');
+searchForm.addEventListener('keyup', (event) => {
+  const input = event.target.value.toLowerCase();
+  const books = bookList.getElementsByTagName('li');
+  Array.from(books).forEach((book) => {
+    const title = book.firstElementChild.textContent;
+    if (title.toLowerCase().indexOf(input) != -1) {
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
+
+
+
